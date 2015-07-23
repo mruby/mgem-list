83116cc14a19b152383ade8014117ce2eb39c5e5
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-07-23 13:13:16 +0900

    check strictly

--- .check.rb
+++ .check.rb
@@ -4,7 +4,12 @@ require 'yaml'
 
 Dir.glob("#{File.dirname __FILE__}/*.gem") do |f|
   print "checking #{f}\n"
-  YAML.parse File.read f
+  tree = YAML.parse_file(f).transform
+  raise "invalid yaml" unless tree["name"] && tree["description"] && tree["author"] && tree["website"] && tree["repository"]
+  raise "invalid protocol" unless ["git"].include? tree["protocol"] # TODO
+  tree["dependencies"].to_a.each do |x|
+    raise "invalid dependencies" unless x.kind_of?(String)
+  end
 end
 
 print "gem files check OK\n"
