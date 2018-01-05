d737f1f881bf7df53338656735ce6a173bb238f9
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-01-05 19:43:23 +0900

    error when the gem not have license

--- .check.rb
+++ .check.rb
@@ -7,6 +7,7 @@ Dir.glob("#{File.dirname __FILE__}/*.gem") do |f|
   tree = YAML.parse_file(f).transform
   raise "invalid yaml" unless tree["name"] && tree["description"] && tree["author"] && tree["website"] && tree["repository"]
   raise "invalid protocol" unless ["git"].include? tree["protocol"] # TODO
+  raise "no license" unless tree["license"]
   tree["dependencies"].to_a.each do |x|
     raise "invalid dependencies" unless x.kind_of?(String)
   end
