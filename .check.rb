69b2dc57285481a9d88cd5b5d83e2d6411b0240e
John Bampton <jbampton@gmail.com>
2021-04-12 04:14:06 +1000

    chore: fix case of YAML

--- .check.rb
+++ .check.rb
@@ -5,7 +5,7 @@ require 'yaml'
 Dir.glob("#{File.dirname __FILE__}/*.gem") do |f|
   print "checking #{f}\n"
   tree = YAML.parse_file(f).transform
-  raise "invalid yaml" unless tree["name"] && tree["description"] && tree["author"] && tree["website"] && tree["repository"]
+  raise "invalid YAML" unless tree["name"] && tree["description"] && tree["author"] && tree["website"] && tree["repository"]
   raise "invalid protocol" unless ["git"].include? tree["protocol"] # TODO
   raise "no license" unless tree["license"]
   tree["dependencies"].to_a.each do |x|
