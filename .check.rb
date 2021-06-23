80d49e9bffa9e0ed05205a0c6f33e329dd76364b
John Bampton <jbampton@gmail.com>
2021-06-23 17:55:50 +1000

    Rubocop format

--- .check.rb
+++ .check.rb
@@ -5,11 +5,14 @@ require 'yaml'
 Dir.glob("#{File.dirname __FILE__}/*.gem") do |f|
   print "checking #{f}\n"
   tree = YAML.parse_file(f).transform
-  raise "invalid YAML" unless tree["name"] && tree["description"] && tree["author"] && tree["website"] && tree["repository"]
-  raise "invalid protocol" unless ["git"].include? tree["protocol"] # TODO
-  raise "no license" unless tree["license"]
-  tree["dependencies"].to_a.each do |x|
-    raise "invalid dependencies" unless x.kind_of?(String)
+  unless tree['name'] && tree['description'] && tree['author'] && tree['website'] && tree['repository']
+    raise 'invalid YAML'
+  end
+  raise 'invalid protocol' unless ['git'].include? tree['protocol'] # TODO
+  raise 'no license' unless tree['license']
+
+  tree['dependencies'].to_a.each do |x|
+    raise 'invalid dependencies' unless x.is_a?(String)
   end
 end
 
