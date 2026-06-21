4c71dccc2b248ec8a66d454b3774f65353922f67
dearblue <dearblue@gmx.com>
2026-06-21 22:36:46 +0900

    Added the `.check.rb --gitrepo` switch
    The purpose is to enable verification of the validity of Git repositories and branches.
    
    It actually accesses the URL specified by the "repository" in the ".gem" file and retrieves the branch and tag names.
    If the "branch" in the ".gem" file is not included in the retrieved reference name, it reports an error.
    Additionally, if the "branch" in the ".gem" file is actually a tag name, it also reports an error.
    
    Furthermore, the `.check.rb --interval` switch has been added.
    The `--gitrepo` switch is equivalent to web scraping from the perspective of the target server.
    Therefore, unlimited consecutive access may be restricted by the target server’s rate limits.
    This switch allows specifying the interval between consecutive accesses as a real number in seconds.
    The default value is 1.2 seconds.

--- .check.rb
+++ .check.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require "optparse"
 require "yaml"
 
 # please add or remove files on the top dir for management if necessary.
@@ -23,6 +24,12 @@ $PROGRAM_NAME = File.basename($PROGRAM_NAME)
 
 def run_and_split_nul(cmd)
   IO.popen(cmd, "rb") { |pipe| pipe.read.split("\0") }
+end
+
+class Array # rubocop:disable Style/Documentation
+  def extract_git_branches
+    grep(%r{^refs/heads/}).map { |e| e.delete_prefix("refs/heads/") }
+  end
 end
 
 errinfo = Object.new
@@ -49,7 +56,62 @@ class << errinfo
   end
 end
 
+def git_get_remote_refs(repository)
+  env = { "GIT_TERMINAL_PROMPT" => "0" }
+  cmd = %W[git ls-remote -btq --refs #{repository}]
+
+  IO.popen(env, cmd, "r", err: File::NULL) do |r|
+    refs = r.read.split(/[\n\t]/).select.with_index { |_, i| i.odd? }
+    return refs unless refs.empty?
+  end
+
+  nil
+end
+
+def check_git_repository(gemname, tree, errinfo) # rubocop:disable Metrics/MethodLength
+  repo = tree["repository"].to_s
+  branch = tree["branch"] || "master"
+  refs = git_get_remote_refs(repo) unless repo.empty?
+
+  if refs.nil?
+    errinfo.push <<~INFO
+      #{gemname}: unable to access the repository or locate a valid branch name.
+    INFO
+  elsif refs.include?("refs/heads/#{branch}")
+    # OK
+  elsif refs.include?("refs/tags/#{branch}")
+    errinfo.push <<~INFO
+      #{gemname}: instead of branches, the tag "#{branch}" is used.
+    INFO
+  else
+    errinfo.push <<~INFO
+      #{gemname}: branch "#{branch}" is missing.
+      > candidates: #{refs.extract_git_branches.join(" ")}
+    INFO
+  end
+end
+
 Dir.chdir(__dir__)
+
+banner = <<~BANNER
+  Usage: #{$PROGRAM_NAME} [options]
+BANNER
+
+gitrepo = false
+interval = 1.2
+OptionParser.new(banner, 24, "") do |o|
+  o.separator ""
+
+  o.on "--gitrepo", "Verify a Git repository and a specified branch" do
+    gitrepo = true
+  end
+
+  o.on "--interval seconds", "Specify the access interval with `--gitrepo` (default: #{interval})", Float do |n|
+    interval = n
+  end
+
+  o.order!
+end
 
 entries = run_and_split_nul(%w[git ls-files -z :^*/*])
 
@@ -61,7 +123,7 @@ unless (PROJECT_FILES - entries).empty?
   MESG
 end
 
-(entries - PROJECT_FILES).each do |f|
+(entries - PROJECT_FILES).each_with_index do |f, i|
   puts "checking #{f}"
 
   unless f.match?(/\.gem$/)
@@ -81,6 +143,11 @@ end
 
   tree["dependencies"].to_a.each do |x|
     errinfo.push "#{f}: invalid dependencies" unless x.is_a?(String)
+  end
+
+  if gitrepo
+    sleep interval if i.positive?
+    check_git_repository(f, tree, errinfo)
   end
 rescue StandardError => e
   errinfo.push e
